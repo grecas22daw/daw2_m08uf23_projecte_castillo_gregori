ec322dffec670f8985c8a27bccfafa7dfc30a6c5
grecas22daw <15583053.clot@fje.edu>
2023-05-10 17:04:52 +0200

    all

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<title>
+	        Index
+		</title>
+	</head>
+	<body>
+		<form action="auth.php" method="POST">
+			Usuari: <input type="text" name="admin"><br>
+			Cont: <input type="password" name="contra"><br>
+			<input type="submit" value="Inicia Sessió" />
+			<input type="reset" value="Buida" />
+		</form>
+	</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<title>
+Menú
+</title>
+</head>
+<body>
+<h2>Menú</h2>
+<a href="vUsuari.php">Visualitza</a> <br>
+<a href="cUsuari.php">Crea</a><br>
+<a href="eUsuari.php">Elimina</a><br>
+<a href="mUsuari.php">Modifica</a><br>
+</body>
+</html>
